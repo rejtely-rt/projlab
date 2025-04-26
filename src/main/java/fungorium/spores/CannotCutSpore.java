9e88b77a70b8d2d5f1721e1d24db2f8a14afd592
Fehér Máté <fehermate@Feher-MacBook-Pro.local>
2025-04-26 18:53:03 +0200

    changes for interpreter

--- src/main/java/fungorium/spores/CannotCutSpore.java
+++ src/main/java/fungorium/spores/CannotCutSpore.java
@@ -1,6 +1,7 @@
 package fungorium.spores;
 
 import fungorium.model.Insect;
+import fungorium.utils.Interpreter;
 import fungorium.utils.Logger;
 
 public class CannotCutSpore extends Spore {
@@ -13,8 +14,6 @@ public class CannotCutSpore extends Spore {
      */
     @Override
     public void applyEffect(Insect insect) {
-        Logger.enter(this, "applyEffect");
         insect.changeCut(false);
-        Logger.exit("");
     }
 }
